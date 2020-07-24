383f4526091f5b335e0612ae2b84fc784efc4266
AuFeld <goldfeld.chase@gmail.com>
2020-07-24 12:52:22 -0400

    SC Complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,19 +4,19 @@ import sys
 
 ''' Opcodes - Command Variables''' 
 
-# halt the cpu and exit the emulator
 HLT = 0b00000001 
-
-# load 'immediate', store a value in register
 LDI = 0b10000010 
-
-# prints the numeric value stored in a register
 PRN = 0b01000111 
 
 POP = 0b01000110
 PUSH = 0b01000101
 CALL = 0b01010000
 RET = 0b00010001
+
+# Add JMP, JEQ, & JNE OpCodes - numeric values stored in register
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
 
 ADD = 0b10100000
 SUB = 0b10100001
@@ -43,9 +43,7 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        # Add list properties to the CPU class to hold 256 
-        # bytes of memory and 8 general purpose registers
-        
+
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.reg[SP] = 0xF4
@@ -61,6 +59,11 @@ class CPU:
             PUSH: self.push,
             CALL: self.call, 
             RET: self.ret, 
+            
+            # add JMP, JEQ, and JNE to the branch table
+            JMP: self.jmp,
+            JEQ: self.jeq,
+            JNE: self.jne,
 
             ADD: self.alu,
             SUB: self.alu,
@@ -82,7 +85,7 @@ class CPU:
         }
 
     def ldi(self, op_a, op_b):
-        ''' sets a specific register to a specific value '''
+        ''' set the value of a register to an integer '''
         self.reg[op_a] = op_b 
     
     def prn(self, op_a, op_b=None):
@@ -94,29 +97,49 @@ class CPU:
         sys.exit()
 
     def pop(self, op_a, op_b=None):
-        ''' removes values from stack using arithmetic logic unit (ALU) '''
+        ''' pop the value at the top of the stack into the given register '''
         self.reg[op_a] = self.ram_read(self.reg[SP])
         self.reg[SP] += 1
 
     def push(self, op_a, op_b=None):
-        ''' adds values to stack using arithmetic logic unit (ALU) ''' 
+        ''' pushes values to stack using arithmetic logic unit (ALU) ''' 
         self.reg[SP] -= 1
         self.ram_write(self.reg[SP], self.reg[op_a])
 
     def call(self, op_a, op_b=None):
-        ''' jumps to address ''' 
-        # push on the stack
+        ''' calls a subroutine (function) at the address stored in register ''' 
         self.reg[SP] -= 1
         self.ram_write(self.reg[SP], self.PC + 2)
-        # set the program counter (PC) to the value in the given register
         self.PC = self.reg[op_a]
     
     def ret(self, op_a=None, op_b=None):
-        ''' returns back to where it was called from '''
-        # pop return address from top of stack
-        # set the program counter (PC)
+        ''' return from subroutine '''
         self.PC = self.ram_read(self.reg[SP])
         self.reg[SP] += 1
+
+    # add jmp method
+    def jmp(self, op_a, op_b=None):
+        ''' jump to the address stored in given register '''
+        # set the PC to the address stored in the given register
+        self.PC = self.reg[op_a]
+
+    # add jeq method
+    def jeq(self, op_a, op_b=None):
+        ''' if 'equal' flag is set, jump to the address stored in the 
+        given register ''' 
+        if self.FL & 1: 
+            self.PC = self.reg[op_a]
+        else:
+            self.PC += 2
+    
+    # add jne method
+    def jne(self, op_a, op_b=None):
+        ''' if 'E' flag is clear (false, 0), jump to the address stored in the
+        given register ''' 
+        if not self.FL & 1:
+            self.PC = self.reg[op_a]
+        else:
+            self.PC += 2
 
     def ram_read(self, address):
         ''' reads the CPU object in RAM ''' 
@@ -140,23 +163,7 @@ class CPU:
                     continue
                 self.ram[address] = instruction
                 address += 1
-        '''
-        # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-        '''
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -188,6 +195,10 @@ class CPU:
         elif op == DEC:
             self.reg[reg_a] -= 1
         elif op == CMP:
+            ''' compares the values in two registers ''' 
+            # if reg_a is greater then reg_b, set greater-than G flag to 1, else 0
+            # if reg_a is less than re_b, set the less-than L flag to 1, else 0
+            # if equal, set the equal flag to 0
             self.FL = ((self.reg[reg_a] < self.reg[reg_b]) << 2) | \
                       ((self.reg[reg_a] > self.reg[reg_b]) << 1) | \
                       ((self.reg[reg_a] == self.reg[reg_b]) << 0)
@@ -202,7 +213,7 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.PC,
-            #self.fl,
+            self.Fl,
             #self.ie,
             self.ram_read(self.PC),
             self.ram_read(self.PC + 1),
@@ -220,7 +231,7 @@ class CPU:
         reads the memory address that's stored in the
         register PC, and store the result in IR (instrction
         register) '''
-        # set running to true
+       
         running = True
 
         while running:
@@ -228,8 +239,6 @@ class CPU:
             op_a = self.ram_read(self.PC + 1)
             op_b = self.ram_read(self.PC + 2)
 
-            # >> bitwise shift right by 'x' places
-            # new bits on the right hand side are zeroes
             num_operands = ir >> 6
 
             PC_set = (ir >> 4) & 1
